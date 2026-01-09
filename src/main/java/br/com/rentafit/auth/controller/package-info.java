f7fe2620dabf882fd08fd472c3512b7f8d6505fe
Richard Calderan <richardcck@hotmail.com>
2026-01-08 22:19:12 -0300

    Refactor NfsePortalClient to use StsTokenService for token management; update tests and documentation to reflect changes

---
+++ src/main/java/br/com/rentafit/auth/controller/package-info.java
@@ -0,0 +1,5 @@
+package br.com.rentafit.auth.controller;
+
+// REST Controllers for authentication endpoints will be placed here
+// Example: AuthController with login, logout, refresh token endpoints
+
